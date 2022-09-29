940fd70aeae911291da52f8e7c602de16b3dee56
Pablo Antuña <pablo.antuna99@gmail.com>
2022-09-29 19:51:02 -0300

    super wip

--- src/Bytecompile.hs
+++ src/Bytecompile.hs
@@ -33,6 +33,20 @@ import Data.Bits (shiftR, Bits (shiftL, (.|.)))
 type Opcode = Word8
 type Bytecode = [Word8]
 
+newtype Bytecode8 = BC {un8 :: [Word8]}
+
+instance Binary Bytecode8 where
+  put (BC bs) = mapM_ putWord8 bs
+  get = go
+    where go =
+           do
+            empty <- isEmpty
+            if empty
+              then return $ BC []
+              else do x <- getWord8
+                      BC xs <- go
+                      return $ BC (x:xs)
+
 data Val = I Int | Fun Env Bytecode | RA Env Bytecode
 type Env = [Val]
 
@@ -101,7 +115,7 @@ bcc (V (p, _) (Global s)) = do
 bcc (Const _ (CNat c)) = return $ CONST : int2bc c
 bcc (Lam _ _ _ (Sc1 t)) = do
   bt <- bcc t
-  return $ [FUNCTION, length bt + 1] ++ bt ++ [RETURN]
+  return $ (FUNCTION : int2bc (length bt + 1)) ++ bt ++ [RETURN]
 bcc (App _ t t') = do
   bt <- bcc t
   bt' <- bcc t'
@@ -113,13 +127,13 @@ bcc (BinaryOp _ Add t t') = (\x y -> x ++ y ++ [ADD]) <$> bcc t <*> bcc t'
 bcc (BinaryOp _ Sub t t') = (\x y -> x ++ y ++ [SUB]) <$> bcc t <*> bcc t'
 bcc (Fix _ _ _ _ _ (Sc2 t)) = do
   bt <- bcc t
-  return $ [FIX, length bt + 1] ++ bt ++ [RETURN]
+  return $ (FIX : int2bc (length bt + 1)) ++ bt ++ [RETURN]
 bcc (IfZ _ cond thenT elseT) = do
   bcond <- bcc cond
   bthen <- bcc thenT
   belse <- bcc elseT
   -- La idea es que JUMP saque lo que está en la pila, y si es distinto de 0 entonces salta tantas posiciones como lo dice en el proximo byte
-  return $ bcond ++ [JUMP, length bthen + 4] ++ bthen ++ [CONST, 1, JUMP, length belse] ++ belse
+  return $ bcond ++ (JUMP : int2bc (length bthen + 4)) ++ bthen ++ (CONST:int2bc 1) ++ (JUMP : int2bc (length belse)) ++ belse
 bcc (Let _ _ _ def (Sc1 body)) = do
   bdef <- bcc def
   bbody <- bcc body
@@ -139,10 +153,12 @@ bc2int = foldr unstep 0
 -- ord/chr devuelven los codepoints unicode, o en otras palabras
 -- la codificación UTF-32 del caracter.
 string2bc :: String -> Bytecode
-string2bc = map ord
+string2bc = concatMap (int2bc . ord)
 
 bc2string :: Bytecode -> String
-bc2string = map chr
+bc2string [] = []
+bc2string (i1:i2:i3:i4:xs) = chr (bc2int [i1, i2, i3, i4]) : bc2string xs
+bc2string _ = error "no es por aca amigo"
 
 global2Free :: Var -> Var
 global2Free (Global s) = Free s
@@ -159,7 +175,7 @@ toLets ((Decl p s ty t) : des) = Let (p, getTy t) s ty t (close s $ toLets des)
 
 -- | Toma un bytecode, lo codifica y lo escribe un archivo
 bcWrite :: Bytecode -> FilePath -> IO ()
-bcWrite bs filename = BS.writeFile filename (encode $ fromIntegral <$> bs)
+bcWrite bs filename = BS.writeFile filename (encode $ BC $ fromIntegral <$> bs)
 
 ---------------------------
 -- * Ejecución de bytecode
@@ -167,7 +183,7 @@ bcWrite bs filename = BS.writeFile filename (encode $ fromIntegral <$> bs)
 
 -- | Lee de un archivo y lo decodifica a bytecode
 bcRead :: FilePath -> IO Bytecode
-bcRead filename = (map fromIntegral <$> id) . decode <$> BS.readFile filename
+bcRead filename = (map fromIntegral <$> un8) . decode <$> BS.readFile filename
 
 runBC :: MonadFD4 m => Bytecode -> m ()
 runBC = runBC' [] []
@@ -175,18 +191,18 @@ runBC = runBC' [] []
 runBC' :: MonadFD4 m => [Val] -> Env -> Bytecode -> m ()
 runBC' s e [] = failFD4 "Che maestro el bytecode me lo mandas por encomienda o que onda?"
 runBC' (v : RA e c : s) _ (RETURN:xs) = runBC' (v : s) e c
-runBC' s e (CONST:i:xs) = runBC' (I i : s) e xs
-runBC' s e (ACCESS:i:xs) = runBC' ((e !! i) : s) e xs
-runBC' s e (FUNCTION:i:xs) = runBC' (Fun e (take i xs) : s) e (drop i xs)
+runBC' s e (CONST:i1:i2:i3:i4:xs) = runBC' (I (bc2int [i1, i2, i3, i4]) : s) e xs
+runBC' s e (ACCESS:i1:i2:i3:i4:xs) = runBC' ((e !! bc2int [i1, i2, i3, i4]) : s) e xs
+runBC' s e (FUNCTION:i1:i2:i3:i4:xs) = runBC' (Fun e (take (bc2int [i1, i2, i3, i4]) xs) : s) e (drop (bc2int [i1, i2, i3, i4]) xs)
 runBC' (v : Fun ef cf : s) e (CALL:xs) = runBC' (RA e xs : s) (v : ef) cf
 runBC' (I y : I x : s) e (ADD:xs) = runBC' (I (semOp Add x y) : s) e xs
 runBC' (I y : I x : s) e (SUB:xs) = runBC' (I (semOp Sub x y) : s) e xs
-runBC' s e (FIX:i:xs) = runBC' (Fun efix (take i xs) : s) e (drop i xs)
-  where efix = Fun efix (take i xs) : e
+runBC' s e (FIX:i1:i2:i3:i4:xs) = runBC' (Fun efix (take (bc2int [i1, i2, i3, i4]) xs) : s) e (drop (bc2int [i1, i2, i3, i4]) xs)
+  where efix = Fun efix (take (bc2int [i1, i2, i3, i4]) xs) : e
 runBC' s e (STOP:xs) = printFD4 "The End"
-runBC' (I n : s) e (JUMP:i:xs) -- Jump es un JNZ
+runBC' (I n : s) e (JUMP:i1:i2:i3:i4:xs) -- Jump es un JNZ
  | n == 0 = runBC' s e xs
- | otherwise = runBC' s e (drop i xs)
+ | otherwise = runBC' s e (drop (bc2int [i1, i2, i3, i4]) xs)
 runBC' (v : s) e (SHIFT:xs) = runBC' s (v : e) xs
 runBC' s (v : e) (DROP:xs) = runBC' s e xs
 runBC' s e (PRINT:xs) = let (str, _:rest) = span (/= NULL) xs in printFD4 (bc2string str) >> runBC' s e rest
