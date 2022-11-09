0eedd52be8f888d0a0892ccbb225d8a20d0bd18d
Pablo Antuña <pablo.antuna99@gmail.com>
2022-11-09 16:59:18 -0300

    tipos correctos y fixeado print y binop

--- src/ClosureConvert.hs
+++ src/ClosureConvert.hs
@@ -14,12 +14,12 @@ freshName s = do
   put (i + 1)
   return (s ++ show i)
 
-toLetsIR :: Name -> [Name] -> Ir -> Ir
+toLetsIR :: Name -> [(Name, Ty)] -> Ir -> Ir
 toLetsIR = go 1
   where
-    go :: Int -> Name -> [Name] -> Ir -> Ir
+    go :: Int -> Name -> [(Name, Ty)] -> Ir -> Ir
     go _ clos [] b = b
-    go i clos (n:ns) b = IrLet n IrInt (IrAccess (IrVar clos) IrInt i) (go (i + 1) clos ns b) -- NO VA IRINT (NO ME IMPORTA)
+    go i clos ((n, ty):ns) b = IrLet n (getTyIR ty) (IrAccess (IrVar clos) (getTyIR ty) i) (go (i + 1) clos ns b) -- NO VA IRINT (NO ME IMPORTA)
 
 substIR :: Name -> Ir -> Ir -> Ir
 substIR n t (IrVar s)
@@ -51,24 +51,32 @@ closureConvert (Const x0 c) = return $ IrConst c
 closureConvert t@(Lam (_, fty) s ty sc) = do
   n <- freshName ("lam_" ++ s)
   clos <- freshName "closure"
-  let freeVs = freeVars t
+  let freeVs = freeVarsWithTypes t
   body <- closureConvert (open s sc)
   tell [IrFun n (getRetTyIR fty) [(clos, IrClo), (s, getTyIR ty)] (toLetsIR clos freeVs body)]
-  return $ MkClosure n (map IrVar freeVs)
+  return $ MkClosure n (map (IrVar . fst) freeVs)
 closureConvert (App (_, ty) t t') = do
   clos <- freshName "app"
   def <- closureConvert t
   arg <- closureConvert t'
   return $ IrLet clos IrClo def (IrCall (IrAccess (IrVar clos) IrFunTy 0) [IrVar clos, arg] (getTyIR ty))
-closureConvert (Print x0 s t) = IrPrint s <$> closureConvert t
-closureConvert (BinaryOp x0 bo t t') = IrBinaryOp bo <$> closureConvert t <*> closureConvert t'
+closureConvert (Print x0 s t) = do
+  nam <- freshName "printN"
+  bbbb <- closureConvert t
+  return $ IrLet nam IrInt bbbb (IrPrint s (IrVar nam))
+closureConvert (BinaryOp x0 bo t t') = do
+  bt <- closureConvert t
+  bt' <- closureConvert t'
+  nameL <- freshName "boL"
+  nameR <- freshName "boR"
+  return $ IrLet nameL (getTyIR $ getTy t) bt (IrLet nameR (getTyIR $ getTy t') bt' $ IrBinaryOp bo (IrVar nameL) (IrVar nameR))
 closureConvert t@(Fix x0 fn fty xn xty sc) = do
   n <- freshName ("fix_" ++ fn ++ "_" ++ xn)
   clos <- freshName "fixClosure"
-  let freeVs = freeVars t
+  let freeVs = freeVarsWithTypes t
   body <- closureConvert (open2 fn xn sc)
   tell [IrFun n (getRetTyIR fty) [(clos, IrClo), (xn, getTyIR xty)] (IrLet fn (getTyIR fty) (IrVar clos) (toLetsIR clos freeVs body))]
-  return $ MkClosure n (map IrVar freeVs)
+  return $ MkClosure n (map (IrVar . fst) freeVs)
 closureConvert (IfZ x0 t t' t'') = IrIfZ <$> closureConvert t <*> closureConvert t' <*> closureConvert t''
 closureConvert (Let (pos, ty) s _ t sc) = IrLet s (getTyIR ty) <$> closureConvert t <*> closureConvert (open s sc)
 
